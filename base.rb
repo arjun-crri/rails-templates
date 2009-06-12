d45aa7f71b462bdb17f23e2d1bf67d147a3a4226
Adam Elliot <adam@warptube.com>
2009-06-11 21:54:59 -0600

    Created base template.

---
+++ base.rb
@@ -0,0 +1,39 @@
+# A base Rails 2.3 configuration
+
+gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com'
+gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
+gem 'jscruggs-metric_fu', :version => '1.0.2', :lib => 'metric_fu', :source => 'http://gems.github.com'
+gem 'haml'
+ 
+rake "gems:install", :sudo => true
+git :init
+
+plugin "db-populate", :git => "git://github.com/ffmike/db-populate.git", :submodule => true
+file "db/populate/.gitignore"
+
+run "haml --rails ."
+run "echo 'TODO Fill this file with some information.' > README.rdoc"
+run "touch tmp/.gitignore vendor/.gitignore public/images/.gitignore"
+run "cp config/database.yml config/database.example.yml"
+run "rm -rf public/index.html public/javascripts"
+run "rm -rf public/images/rails.png"
+
+# Download JQuery
+run 'curl http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js > public/javascripts/jquery.js'
+
+file "public/javascripts/application.js"
+file "public/stylesheets/sass/application.sass"
+file "config/initializers/haml.rb", <<-HAML
+Haml::Template::options[:attr_wrapper] = '"'
+Sass::Plugin.options[:style] = :compressed
+HAML
+  
+file ".gitignore", <<-IGNORE
+log/*.log
+tmp/**/*
+config/database.yml
+db/*.sqlite3
+db/*.db
+IGNORE
+
+git :add => ".", :commit => '-m "Initial commit."'
